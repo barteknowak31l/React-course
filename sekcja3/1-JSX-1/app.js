6507167350b49187530bef59b83629ca3fc5ce41
Bartek Nowak <barteknowak31l@gmail.com>
2024-01-27 13:36:01 +0100

    simple class component usage

--- sekcja3/1-JSX-1/app.js
+++ sekcja3/1-JSX-1/app.js
@@ -1,51 +1,36 @@
-// Komponenty
-
-// funkcyjny - bezstanowy
-const Header = () => {
-  return (
-    <div>
-      <h1>Witaj na stronie!</h1>
-    </div>
-  );
-};
-
-// klasowy - stanowy
-class Blog extends React.Component {
+class ShoppingList extends React.Component {
   state = {
-    number: 0,
+    items1: "ogórki",
+    items2: "sok",
+    items3: "dżem",
   };
 
   render() {
     return (
-      <section>
-        <h2>Artykuł {this.state.number}</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt
-          ornare massa eget egestas. Nullam non nisi est sit amet facilisis
-          magna. Dui vivamus arcu felis bibendum ut tristique et egestas quis.
-          Id porta nibh venenatis cras sed felis. Sit amet facilisis magna etiam
-          tempor orci. Nulla at volutpat diam ut venenatis tellus in metus.
-          Elementum tempus egestas sed sed risus pretium. Etiam erat velit
-          scelerisque in dictum. Dolor morbi non arcu risus quis varius quam
-          quisque. Elit ullamcorper dignissim cras tincidunt lobortis feugiat
-          vivamus at augue. Erat velit scelerisque in dictum non. Rhoncus dolor
-          purus non enim praesent. Quisque non tellus orci ac auctor. Pharetra
-          et ultrices neque ornare aenean euismod elementum. Vitae sapien
-          pellentesque habitant morbi tristique senectus.
-        </p>
-      </section>
+      <>
+        <h1>Lista zakupów</h1>
+        <ul>
+          <ItemList name={this.state.items1} example="4" />
+          <ItemList name={this.state.items2} />
+          <ItemList name={this.state.items3} />
+        </ul>
+      </>
     );
   }
 }
 
-const App = () => {
-  return (
-    <>
-      <Header />
-      <Blog />
-    </>
-  );
-};
+// const ItemList = (props) => {
+//   return <li>{props.name}</li>;
+// };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+class ItemList extends React.Component {
+  render() {
+    return (
+      <li>
+        {this.props.name} - {this.props.example}
+      </li>
+    );
+  }
+}
+
+ReactDOM.render(<ShoppingList />, document.getElementById("root"));
