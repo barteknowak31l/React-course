3f9b2558389fe2d9a86e21a03b70427abb03c6a0
Bartek Nowak <barteknowak31l@gmail.com>
2024-01-27 13:09:30 +0100

    basic reactDOM usage

--- sekcja3/1-JSX-1/app.js
+++ sekcja3/1-JSX-1/app.js
@@ -1,12 +1,39 @@
-//console.log(React);
-//console.log(ReactDOM);
+const header = <h1 className="title">Witaj!</h1>;
 
-const element = <div>Pierwszy element React</div>;
+const classBig = "big";
 
-const element2 = React.createElement("div", null, "Pierwszy element React");
+const handleClick = () => alert("Click!");
 
-const element3 = (
+const main = (
   <div>
-    <p>Tekst</p>
+    <h1 onClick={handleClick} className="red">
+      Pierwszy artykul
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Tincidunt ornare massa
+      eget egestas. Nullam non nisi est sit amet facilisis magna. Dui vivamus
+      arcu felis bibendum ut tristique et egestas quis. Id porta nibh venenatis
+      cras sed felis. Sit amet facilisis magna etiam tempor orci. Nulla at
+      volutpat diam ut venenatis tellus in metus. Elementum tempus egestas sed
+      sed risus pretium. Etiam erat velit scelerisque in dictum. Dolor morbi non
+      arcu risus quis varius quam quisque. Elit ullamcorper dignissim cras
+      tincidunt lobortis feugiat vivamus at augue. Erat velit scelerisque in
+      dictum non. Rhoncus dolor purus non enim praesent. Quisque non tellus orci
+      ac auctor. Pharetra et ultrices neque ornare aenean euismod elementum.
+      Vitae sapien pellentesque habitant morbi tristique senectus.
+    </p>
   </div>
 );
+
+const footerText = "FooTeer";
+
+const footer = (
+  <footer>
+    <p className={classBig}>{footerText}</p>
+  </footer>
+);
+
+const app = [header, main, footer];
+
+ReactDOM.render(app, document.getElementById("root"));
