d172803a706a635bb0e42eb2506659a56023c9d7
Bartek Nowak <barteknowak31l@gmail.com>
2024-01-28 00:25:28 +0100

    react app timer with more components

--- sekcja7_ReactApp/my-app/src/components/App.js
+++ sekcja7_ReactApp/my-app/src/components/App.js
@@ -1,9 +1,38 @@
 import React, { Component } from "react";
 import "./App.css";
+import SwitchButton from "./SwitchButton";
 
 class App extends Component {
+  state = {
+    time: 0,
+    active: false,
+  };
+
+  addSecond = () => {
+    this.setState({
+      time: this.state.time + 1,
+    });
+  };
+
+  handleClick = () => {
+    if (this.state.active) {
+      clearInterval(this.idInterval);
+    } else {
+      this.idInterval = setInterval(() => this.addSecond(), 1000);
+    }
+
+    this.setState({
+      active: !this.state.active,
+    });
+  };
+
   render() {
-    return <div className="app">Aplikacja</div>;
+    return (
+      <>
+        <p>{this.state.time}</p>
+        <SwitchButton click={this.handleClick} active={this.state.active} />
+      </>
+    );
   }
 }
 
