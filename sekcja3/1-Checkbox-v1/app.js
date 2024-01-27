d5ea4d5e1b590de85390248e8ac465f093164b9a
Bartek Nowak <barteknowak31l@gmail.com>
2024-01-27 21:21:55 +0100

    task1: fortune teller

--- sekcja3/1-Checkbox-v1/app.js
+++ sekcja3/1-Checkbox-v1/app.js
@@ -1,127 +1,74 @@
-// kantor demo
-
-// const Dollars = (props) => {
-//   const value = (props.cash / props.ratio).toFixed(2);
-//   return <div>Wartość w Dolarach: {props.cash <= 0 ? " " : value}</div>;
-// };
-// const Euros = (props) => {
-//   const value = (props.cash / props.ratio).toFixed(2);
-//   return <div>Wartość w Euro: {props.cash <= 0 ? " " : value}</div>;
-// };
-
-const Cash = (props) => {
-  const value = ((props.cash / props.ratio) * props.price).toFixed(2);
-  return (
-    <div>
-      {props.title} {props.cash <= 0 ? " " : value}
-    </div>
-  );
-};
-
-class ExchangeCounter extends React.Component {
+class FortuneTeller extends React.Component {
   state = {
-    amount: "",
-    product: "gas",
-    // ratioDollar: 3.6,
-    // ratioEuro: 4.2,
-  };
+    currentFortune: "",
+    newFortune: "",
 
-  static defaultProps = {
-    currencies: [
-      {
-        id: 0,
-        name: "zloty",
-        ratio: "1.0",
-        title: "Wartość w złotówkach: ",
-      },
+    fortunes: [
       {
         id: 1,
-        name: "dolar",
-        ratio: "3.6",
-        title: "Wartość w dolarach: ",
+        text: "Wróżba nr 1",
       },
       {
         id: 2,
-        name: "euro",
-        ratio: "4.2",
-        title: "Wartość w euro: ",
+        text: "Wróżba nr 2",
       },
       {
         id: 3,
-        name: "pound",
-        ratio: "4.55",
-        title: "Wartość w funtach: ",
+        text: "Wróżba nr 3",
       },
     ],
-
-    prices: {
-      electricity: 0.51,
-      gas: 4.76,
-      oranges: 3.79,
-    },
   };
 
-  handleChange = (e) => {
+  handleClick = () => {
+    const { fortunes } = this.state;
+    const number = Math.floor(Math.random() * fortunes.length) + 1;
+    const chosenFortune = fortunes.filter((fortune) => fortune.id === number);
+    const txt = chosenFortune[0].text;
     this.setState({
-      amount: e.target.value,
+      currentFortune: txt,
     });
   };
 
-  handleProductChange = (e) => {
+  handleNewFortune = (e) => {
     this.setState({
-      product: e.target.value,
-      amount: "",
+      newFortune: e.target.value,
     });
   };
 
-  insertSuffix = (select) => {
-    if (select === "electricity") return <em>kWh</em>;
-    else if (select === "gas") return <em>litrów</em>;
-    else if (select === "oranges") return <em>kg</em>;
-    else return null;
-  };
+  handleAddFortune = () => {
+    const newID = this.state.fortunes.length + 1;
+    const newText = this.state.newFortune;
 
-  selectPrice = (product) => {
-    return this.props.prices[product];
+    const newFortune = {
+      id: newID,
+      text: newText,
+    };
+
+    this.setState({
+      fortunes: [...this.state.fortunes, newFortune],
+      newFortune: "",
+      currentFortune: "",
+    });
   };
 
   render() {
-    const { amount, product } = this.state;
-    const price = this.selectPrice(product);
-
-    const calculators = this.props.currencies.map((currency) => (
-      <Cash
-        key={currency.id}
-        ratio={currency.ratio}
-        title={currency.title}
-        cash={amount}
-        price={price}
-      />
-    ));
-
+    const { currentFortune } = this.state;
     return (
-      <div className="app">
-        <label>
-          Wybierz produkt
-          <select value={product} onChange={this.handleProductChange}>
-            <option value="electricity">prąd</option>
-            <option value="gas">benzyna</option>
-            <option value="oranges"> pomarańcze</option>
-          </select>
-        </label>
+      <>
+        <button onClick={this.handleClick}>Zobacz wróżbę</button>
         <br />
         <label>
           <input
-            type="Number"
-            value={this.state.amount}
-            onChange={this.handleChange}
+            type="text"
+            value={this.state.newFortune}
+            onChange={this.handleNewFortune}
           ></input>
-          {this.insertSuffix(product)}
+          <button onClick={this.handleAddFortune}>Dodaj nową wróżbę</button>
         </label>
-        {calculators}
-      </div>
+        <h1>{currentFortune}</h1>
+      </>
     );
   }
 }
 
-ReactDOM.render(<ExchangeCounter />, document.getElementById("root"));
+ReactDOM.render(<FortuneTeller />, document.getElementById("root"));
