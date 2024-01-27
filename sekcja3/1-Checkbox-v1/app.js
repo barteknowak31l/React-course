a7c25d835600c347effa2f5d8c5165c6c998b3da
Bartek Nowak <barteknowak31l@gmail.com>
2024-01-27 19:59:50 +0100

    possibility for expanding with more currencies

--- sekcja3/1-Checkbox-v1/app.js
+++ sekcja3/1-Checkbox-v1/app.js
@@ -1,75 +1,82 @@
-// form demo
-class Form extends React.Component {
+// kantor demo
+
+// const Dollars = (props) => {
+//   const value = (props.cash / props.ratio).toFixed(2);
+//   return <div>Wartość w Dolarach: {props.cash <= 0 ? " " : value}</div>;
+// };
+// const Euros = (props) => {
+//   const value = (props.cash / props.ratio).toFixed(2);
+//   return <div>Wartość w Euro: {props.cash <= 0 ? " " : value}</div>;
+// };
+
+const Cash = (props) => {
+  const value = (props.cash / props.ratio).toFixed(2);
+  return (
+    <div>
+      {props.title} {props.cash <= 0 ? " " : value}
+    </div>
+  );
+};
+
+class ExchangeCounter extends React.Component {
   state = {
-    city: "Londyn",
-    text: "",
-    isLoved: true,
-    numerOfVisits: 0,
+    amount: "",
+    // ratioDollar: 3.6,
+    // ratioEuro: 4.2,
   };
 
+  currencies = [
+    {
+      id: 1,
+      name: "dolar",
+      ratio: "3.6",
+      title: "Wartość w dolarach: ",
+    },
+    {
+      id: 2,
+      name: "euro",
+      ratio: "4.2",
+      title: "Wartość w euro: ",
+    },
+    {
+      id: 3,
+      name: "pound",
+      ratio: "4.55",
+      title: "Wartość w funtach: ",
+    },
+  ];
+
   handleChange = (e) => {
-    if (e.target.type === "checkbox") {
-      this.setState({
-        [e.target.name]: e.target.checked,
-      });
-    } else {
-      this.setState({
-        [e.target.name]: e.target.value,
-      });
-    }
+    this.setState({
+      amount: e.target.value,
+    });
   };
 
   render() {
+    const { amount, ratioDollar, ratioEuro } = this.state;
+
+    const calculators = this.currencies.map((currency) => (
+      <Cash
+        key={currency.id}
+        ratio={currency.ratio}
+        title={currency.title}
+        cash={amount}
+      />
+    ));
+
     return (
-      <>
-        <div>
-          <label>
-            Podaj miasto
-            <input
-              name="city"
-              type="text"
-              onChange={this.handleChange}
-              value={this.state.city}
-            ></input>
-          </label>
-          <br />
-          <label>
-            Napisz coś o tym mieście
-            <textarea
-              name="text"
-              value={this.state.text}
-              onChange={this.handleChange}
-            ></textarea>
-          </label>
-          <br />
-          <label>
-            Czy lubisz to miasto?
-            <input
-              name="isLoved"
-              type="checkbox"
-              checked={this.state.isLoved}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Ile razy byliście w tym mieście?
-            <select
-              name="numerOfVisits"
-              value={this.state.numerOfVisits}
-              onChange={this.handleChange}
-            >
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="more">więcej</option>
-            </select>
-          </label>
-        </div>
-      </>
+      <div className="app">
+        <label>
+          <input
+            type="Number"
+            value={this.state.amount}
+            onChange={this.handleChange}
+          ></input>
+          {calculators}
+        </label>
+      </div>
     );
   }
 }
 
-ReactDOM.render(<Form />, document.getElementById("root"));
+ReactDOM.render(<ExchangeCounter />, document.getElementById("root"));
