1b4fd1af257731c6fd572feca4e9b9520f20d28c
Bartek Nowak <barteknowak31l@gmail.com>
2024-01-27 13:21:41 +0100

    functional and class components

--- sekcja3/1-JSX-1/app.js
+++ sekcja3/1-JSX-1/app.js
@@ -1,39 +1,51 @@
-const header = <h1 className="title">Witaj!</h1>;
+// Komponenty
 
-const classBig = "big";
+// funkcyjny - bezstanowy
+const Header = () => {
+  return (
+    <div>
+      <h1>Witaj na stronie!</h1>
+    </div>
+  );
+};
 
-const handleClick = () => alert("Click!");
+// klasowy - stanowy
+class Blog extends React.Component {
+  state = {
+    number: 0,
+  };
 
-const main = (
-  <div>
-    <h1 onClick={handleClick} className="red">
-      Pierwszy artykul
-    </h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Tincidunt ornare massa
-      eget egestas. Nullam non nisi est sit amet facilisis magna. Dui vivamus
-      arcu felis bibendum ut tristique et egestas quis. Id porta nibh venenatis
-      cras sed felis. Sit amet facilisis magna etiam tempor orci. Nulla at
-      volutpat diam ut venenatis tellus in metus. Elementum tempus egestas sed
-      sed risus pretium. Etiam erat velit scelerisque in dictum. Dolor morbi non
-      arcu risus quis varius quam quisque. Elit ullamcorper dignissim cras
-      tincidunt lobortis feugiat vivamus at augue. Erat velit scelerisque in
-      dictum non. Rhoncus dolor purus non enim praesent. Quisque non tellus orci
-      ac auctor. Pharetra et ultrices neque ornare aenean euismod elementum.
-      Vitae sapien pellentesque habitant morbi tristique senectus.
-    </p>
-  </div>
-);
+  render() {
+    return (
+      <section>
+        <h2>Artykuł {this.state.number}</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt
+          ornare massa eget egestas. Nullam non nisi est sit amet facilisis
+          magna. Dui vivamus arcu felis bibendum ut tristique et egestas quis.
+          Id porta nibh venenatis cras sed felis. Sit amet facilisis magna etiam
+          tempor orci. Nulla at volutpat diam ut venenatis tellus in metus.
+          Elementum tempus egestas sed sed risus pretium. Etiam erat velit
+          scelerisque in dictum. Dolor morbi non arcu risus quis varius quam
+          quisque. Elit ullamcorper dignissim cras tincidunt lobortis feugiat
+          vivamus at augue. Erat velit scelerisque in dictum non. Rhoncus dolor
+          purus non enim praesent. Quisque non tellus orci ac auctor. Pharetra
+          et ultrices neque ornare aenean euismod elementum. Vitae sapien
+          pellentesque habitant morbi tristique senectus.
+        </p>
+      </section>
+    );
+  }
+}
 
-const footerText = "FooTeer";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Blog />
+    </>
+  );
+};
 
-const footer = (
-  <footer>
-    <p className={classBig}>{footerText}</p>
-  </footer>
-);
-
-const app = [header, main, footer];
-
-ReactDOM.render(app, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
