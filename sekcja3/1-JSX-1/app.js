7ac4173a831a135f56a0a828777d12f076dd0ed2
Bartek Nowak <barteknowak31l@gmail.com>
2024-01-27 14:06:28 +0100

    more exercises on class components

--- sekcja3/1-JSX-1/app.js
+++ sekcja3/1-JSX-1/app.js
@@ -1,36 +1,48 @@
-// po kliknięciu przycisku dodawana jest litera do tekstu
+// po kliknięciu przycisku dodawana jest losowa liczba (0-10)
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    // w state przechowujemy rzeczy, ktore sie beda zmieniac
     this.state = {
       text: "",
     };
-
-    this.buttonHandle = this.buttonHandle.bind(this);
   }
 
   // metoda z prototypu (trzeba ją zbindować dla każdego obiektu)
   // można w konstruktorze, można przy każdym wywołaniu, można użyć funkcji strzałkowej (dziedziczy this po elemencie nadrzeędnym)
-  buttonHandle() {
-    const letter = "a";
+  buttonHandle = () => {
+    const number = Math.floor(Math.random() * 10);
     // this.setState({
     //   text: this.state.text + letter,
     // });
 
     this.setState(() => ({
-      text: this.state.text + letter,
+      text: this.state.text + number,
     }));
-  }
+  };
 
   render() {
+    const btnName = "Stworz liczbe"; // tu tez mozna definiowac zmienne
     return (
       <>
-        <button onClick={this.buttonHandle}>Dodaj A</button>
-        <h1>{this.state.text}</h1>
+        <button onClick={this.buttonHandle}>{this.props.btnTitle}</button>
+        <ResultPanel text={this.state.text}>To jest child</ResultPanel>
       </>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const ResultPanel = (props) => {
+  return (
+    <h1>
+      {props.text}:{props.children}
+    </h1>
+  );
+};
+
+ReactDOM.render(
+  <App btnTitle="Dodaj cyfre" />,
+  document.getElementById("root")
+);
