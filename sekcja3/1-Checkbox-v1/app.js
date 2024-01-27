188cd7f5efff68af316990b8400f8411fdaf03de
Bartek Nowak <barteknowak31l@gmail.com>
2024-01-27 18:09:21 +0100

    improve array demo, list item as component, array of componens

--- sekcja3/1-Checkbox-v1/app.js
+++ sekcja3/1-Checkbox-v1/app.js
@@ -1,16 +1,18 @@
+const Item = (props) => <li>{`item: ${props.content}`}</li>;
+
 class ListItems extends React.Component {
   state = {
     items: ["kaczka", "dżem", "ogórek"],
   };
 
   render() {
+    const items = this.state.items.map((item) => (
+      <Item key={item} content={item} />
+    ));
+
     return (
       <>
-        <ul>
-          {this.state.items.map((item) => (
-            <li key={item}>{`item: ${item}`}</li>
-          ))}
-        </ul>
+        <ul>{items}</ul>
       </>
     );
   }
