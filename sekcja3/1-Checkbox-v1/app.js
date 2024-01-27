71d76a7c52bd884f5fabf22f2cd768ea9f37f405
Bartek Nowak <barteknowak31l@gmail.com>
2024-01-27 18:39:37 +0100

    add array filtering

--- sekcja3/1-Checkbox-v1/app.js
+++ sekcja3/1-Checkbox-v1/app.js
@@ -1,7 +1,9 @@
 const Item = ({ content }) => (
   <div>
     <h1>Użytkownik: {content.name}</h1>
-    <h2>ma: {content.age} lat(a)</h2>
+    <p>Informacje o użytkowniku</p>
+    <p>ma: {content.age} lat(a)</p>
+    <p>płeć: {content.sex}</p>
   </div>
 );
 
@@ -11,28 +13,63 @@ const data = {
       id: 1,
       age: 29,
       name: "Arek",
+      sex: "male",
     },
     {
       id: 2,
       age: 39,
       name: "Darek",
+      sex: "male",
     },
     {
       id: 3,
       age: 49,
       name: "Marek",
+      sex: "male",
+    },
+    {
+      id: 4,
+      age: 19,
+      name: "Marta",
+      sex: "female",
     },
   ],
 };
 
 class ListItems extends React.Component {
-  render() {
-    const users = this.props.data.users;
-    const items = users.map((user) => <Item key={user.id} content={user} />);
+  state = { sex: "none" };
 
+  usersList = () => {
+    let users = this.props.data.users;
+
+    switch (this.state.sex) {
+      case "none":
+        return users.map((user) => <Item key={user.id} content={user} />);
+      case "male":
+        users = users.filter((user) => user.sex === this.state.sex);
+        return users.map((user) => <Item key={user.id} content={user} />);
+      case "female":
+        users = users.filter((user) => user.sex === this.state.sex);
+        return users.map((user) => <Item key={user.id} content={user} />);
+    }
+
+    users = users.map((user) => <Item key={user.id} content={user} />);
+    return users;
+  };
+
+  handleClick = (s) => {
+    this.setState({
+      sex: s,
+    });
+  };
+
+  render() {
     return (
       <>
-        <ul>{items}</ul>
+        <button onClick={() => this.handleClick("none")}>Wszyscy</button>
+        <button onClick={() => this.handleClick("female")}>Kobiety</button>
+        <button onClick={() => this.handleClick("male")}>Mężczyźni</button>
+        {this.usersList()}
       </>
     );
   }
