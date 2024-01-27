b43521d2199521e8894bc8d7d3cac03a1985b7a6
Bartek Nowak <barteknowak31l@gmail.com>
2024-01-27 15:08:08 +0100

    passing args to handlers, intializing components wih props

--- sekcja3/1-JSX-1/app.js
+++ sekcja3/1-JSX-1/app.js
@@ -1,35 +1,41 @@
-class Message extends React.Component {
-  constructor(props) {
-    super(props);
+class Counter extends React.Component {
+  state = {
+    count: 0,
+    result: this.props.result,
+  };
 
-    this.state = {
-      messageIsActive: false,
-    };
-
-    this.handleMessageButton = this.handleMessageButton.bind(this);
-  }
-
-  // użycie setState jako funkji strzełkowej pozwala na poprawne
-  // obsłguiwanie asynchroniczności
-  handleMessageButton() {
-    this.setState((prevState) => ({
-      messageIsActive: !prevState.messageIsActive,
-    }));
+  handleClick(type, number = 1) {
+    if (type === "sub") {
+      this.setState((prevState) => ({
+        count: prevState.count + 1,
+        result: prevState.result - number,
+      }));
+    } else if (type === "reset") {
+      this.setState((prevState) => ({
+        count: prevState.count + 1,
+        result: 0,
+      }));
+    } else if (type === "add") {
+      this.setState((prevState) => ({
+        count: prevState.count + 1,
+        result: prevState.result + number,
+      }));
+    }
   }
 
   render() {
-    const text = "Lorem ipsum";
-    debugger;
     return (
       <>
-        <button onClick={this.handleMessageButton}>
-          {this.state.messageIsActive ? "Ukryj" : "Pokaż"}
-        </button>
-        {this.state.messageIsActive ? <p>{text}</p> : null}
-        {this.state.messageIsActive && <p>{text}</p>}
+        <button onClick={this.handleClick.bind(this, "sub", 10)}>-10</button>
+        <button onClick={() => this.handleClick("sub")}>-1</button>
+        <button onClick={this.handleClick.bind(this, "reset")}>Reset</button>
+        <button onClick={() => this.handleClick("add")}>+1</button>
+        <button onClick={this.handleClick.bind(this, "add", 10)}>+10</button>
+        <h1>Liczba kliknięć: {this.state.count}</h1>
+        <h1>Wynik: {this.state.result}</h1>
       </>
     );
   }
 }
 
-ReactDOM.render(<Message />, document.getElementById("root"));
+ReactDOM.render(<Counter result={0} />, document.getElementById("root"));
