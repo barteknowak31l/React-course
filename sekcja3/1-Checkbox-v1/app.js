4459609bd22a0b239861ba596e1b4410853bfbf5
Bartek Nowak <barteknowak31l@gmail.com>
2024-01-27 18:04:45 +0100

    entry for arrays in react

--- sekcja3/1-Checkbox-v1/app.js
+++ sekcja3/1-Checkbox-v1/app.js
@@ -1,59 +1,19 @@
-// rozszerzenie poprzedniego projektu o przycisk 'kup'
-
-class App extends React.Component {
+class ListItems extends React.Component {
   state = {
-    availbleProducts: 7,
-    shoppingCart: 0,
-  };
-
-  handleRemoveFromCart = () => {
-    this.setState({
-      shoppingCart: this.state.shoppingCart - 1,
-    });
-  };
-
-  addToCart = () => {
-    this.setState({
-      shoppingCart: this.state.shoppingCart + 1,
-    });
-  };
-
-  handleBuy = () => {
-    this.setState({
-      shoppingCart: 0,
-      availbleProducts: this.state.availbleProducts - this.state.shoppingCart,
-    });
+    items: ["kaczka", "dżem", "ogórek"],
   };
 
   render() {
-    const style =
-      this.state.shoppingCart === 0
-        ? {
-            opacity: 0.3,
-          }
-        : {};
-
-    const { shoppingCart, availbleProducts } = this.state;
-
     return (
       <>
-        <button
-          disabled={shoppingCart ? false : true}
-          onClick={this.handleRemoveFromCart}
-        >
-          -
-        </button>
-        <span style={style}> {shoppingCart} </span>
-        <button
-          disabled={shoppingCart >= availbleProducts}
-          onClick={this.addToCart}
-        >
-          +
-        </button>
-        {shoppingCart > 0 && <button onClick={this.handleBuy}>Kup</button>}
+        <ul>
+          {this.state.items.map((item) => (
+            <li key={item}>{`item: ${item}`}</li>
+          ))}
+        </ul>
       </>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ListItems />, document.getElementById("root"));
